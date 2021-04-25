ccba533799496ba4bab3066332252a8033f92638
darhmiee <damio.ogunnaike@gmail.com>
2021-04-25 13:58:37 +0100

    Initial commit

---
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var text = findViewById<TextView>(R.id.textView)
+        var plus = findViewById<Button>(R.id.increment_button)
+        var counter: Int = 0
+
+        plus.setOnClickListener {
+            counter++
+            text.text = counter.toString()
+        }
+        }
+    }
